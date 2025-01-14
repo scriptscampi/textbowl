21d7cf63fb0d1ceb139d5e3ed8dab60a992c25b0
scriptscampi <royalty_happier00@icloud.com>
2025-01-14 17:41:20 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,13 +2,27 @@ import { CONFIG } from "./config.js";
 import { handlePlay, gameState, fireworks } from "./gameLogic.js";
 import { renderGameBoard } from "./ui.js";
 
+//document.getElementById("game-controls").addEventListener("click", handleControlEvent);
+//document.getElementById("game-controls").addEventListener("touchstart", handleControlEvent);
+/**
+ * Randomly selects a team name from the TEAMS object.
+ * @returns {string} - Random team name.
+ */
+function getRandomOpponent() {
+  const teamNames = Object.keys(CONFIG.TEAMS);
+  const randomIndex = Math.floor(Math.random() * teamNames.length);
+  return teamNames[randomIndex];
+}
+
+/**
 
 /**
  * Initializes the game by rendering the initial board and setting up button listeners.
  */
 function initializeGame() {
+  //renderGameBoard("Welcome to Retro Football! Make your plays to outscore the CPU!");
   CONFIG.OPPONENT = getRandomOpponent(); // Set opponent dynamically
-  renderGameBoard("Welcome to Retro Football! Make your plays to outscore the CPU!"); // Render initial state
+  renderGameBoard("Welcome to Text Bowl Football! Make your plays to outscore the CPU!");
 
   const gameControls = document.getElementById("game-controls");
   gameControls.addEventListener("click", (event) => {
@@ -29,38 +43,6 @@ function initializeGame() {
   });
 }
 
-
-/**
- * Randomly selects a team name from the TEAMS object.
- * @returns {string} - Random team name.
- */
-function getRandomOpponent() {
-  const teamNames = Object.keys(CONFIG.TEAMS);
-  const randomIndex = Math.floor(Math.random() * teamNames.length);
-  return teamNames[randomIndex];
-}
-document.getElementById("game-controls").addEventListener("click", handleControlEvent);
-document.getElementById("game-controls").addEventListener("touchstart", handleControlEvent);
-
-function handleControlEvent(event) {
-  const play = parseInt(event.target.dataset.play, 10);
-
-  if (!isNaN(play)) {
-    if (play === 5) {
-      resetGame();
-    } else {
-      handlePlay(play);
-
-      // Disable controls if the game ends
-      if (gameState.timeRemaining === 0 && gameState.quarter === 4) {
-        disableControls();
-      }
-    }
-  }
-}
-/**
- * Enables all game controls except those explicitly excluded.
- */
 function enableControls() {
   const gameControls = document.querySelectorAll("#game-controls button");
   gameControls.forEach((button) => {
@@ -105,3 +87,6 @@ function resetGame() {
 
 // Initialize the game once the DOM content is loaded
 document.addEventListener("DOMContentLoaded", initializeGame);
+
+
+
