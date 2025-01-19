0ffe9f06d610650209f8fb07b63cc937cdc741e5
scriptscampi <royalty_happier00@icloud.com>
2025-01-19 17:24:58 -0600

    Create schema.js

---
+++ js/schema.js
@@ -0,0 +1,14 @@
+{
+  "@context": "https://schema.org",
+  "@type": "Game",
+  "name": "Text Bowl Football Game",
+  "description": "A retro-style text-based football simulator. Make plays, score touchdowns, and relive football history!",
+  "genre": "Sports",
+  "publisher": "SriptScampi",
+  "offers": {
+    "@type": "Offer",
+    "url": "https://textbowl.com",
+    "price": "0.00",
+    "priceCurrency": "USD"
+  }
+}
